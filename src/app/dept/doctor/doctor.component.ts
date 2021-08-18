ff94ce5a2a4810990bca1155559507e4dc2bb8ae
Apakorn <pae.apakorn@gmail.com>
2021-08-18 22:18:54 +0700

    doctor

--- src/app/dept/doctor/doctor.component.ts
+++ src/app/dept/doctor/doctor.component.ts
@@ -6,6 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./doctor.component.css']
 })
 export class DoctorComponent implements OnInit {
+  data = [
+    {
+      datein: '15/6/2021',
+      id: 1,
+      refid: 0,
+      refidbydr: 3,
+      refcombined: 'X',
+    },
+    {
+      datein: '15/6/2021',
+      id: 2,
+      refid: 1,
+      refidbydr: 4,
+      refcombined: 'B'
+    },
+    {
+      datein: '15/6/2021',
+      id: 3,
+      refid: 1,
+      refidbydr: 5,
+      refcombined: 'C'
+    }
+  ]
 
   constructor() { }
 
