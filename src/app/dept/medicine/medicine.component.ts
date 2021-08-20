07a0593747cf57f22ca59da855c63d704bd3b72b
Apakorn <pae.apakorn@gmail.com>
2021-08-20 15:26:49 +0700

    medicine

--- src/app/dept/medicine/medicine.component.ts
+++ src/app/dept/medicine/medicine.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilService } from 'src/app/services/util.service';
+import { Medicine } from './medicine.model';
 
 @Component({
   selector: 'app-medicine',
@@ -6,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./medicine.component.css']
 })
 export class MedicineComponent implements OnInit {
+  listData: Medicine[] = []
 
-  constructor() { }
+  constructor(public util: UtilService) { 
+    var medicine1 = new Medicine(1)
+    medicine1.PRE_ID = 1
+    medicine1.PAT_ID = "PT0005"
+    medicine1.PRE_DATE = new Date()
+    medicine1.PHA_ID = "PH0004"
+    medicine1.PHA_DATE = new Date()
+    medicine1.PHA_CONFIRM = "Confirmed with edited"
+    medicine1.PHA_EDIT = "Off favi"
+
+    this.listData.push(medicine1)
+  }
 
   ngOnInit(): void {
   }
